d619848045e530344407891c6081b54fd78343c2
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2023-06-03 09:42:51 -0400

    Add files via upload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,8 +6,10 @@ options.addEventListener('click', ()=>{
     if(bachiller.checked){
         season_placeholder.max = 5
         season_placeholder.placeholder = "Año";
+        bachiller.value = "Bachiller";
     }else{
         season_placeholder.max = 10
         season_placeholder.placeholder = "Semestre";
+        bachiller.value = "Universitario";
     }
 });
