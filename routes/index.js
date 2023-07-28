8affd0824f88fcbb51c73efb7deae001bc4a1db1
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2023-07-28 17:07:24 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -80,7 +80,8 @@ router.post('/', (req, res) => {
 
     var transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: 587,
+        secure: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
